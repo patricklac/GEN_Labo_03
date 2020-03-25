211ccc9a65e7ae226226f609ba0544b56c5b467f
Corentin Zeller <contact@corentin-zeller.ch>
2020-03-25 15:26:35 +0100

    test Transfert  + assert consistent

--- lecteursredacteurs/src/test/java/TransfertTest.java
+++ lecteursredacteurs/src/test/java/TransfertTest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -19,9 +20,9 @@ public class TransfertTest {
     }
 
     /**
-    Dans une classe de test Junit, déclencher 1000 exécutions de la classe Transfert, chacune réalisant
-    1000 transferts, dans une banque de 10 comptes. Ne pas traiter l’accès concurrent aux comptes par
-    les threads pour l’instant
+     * Dans une classe de test Junit, déclencher 1000 exécutions de la classe Transfert, chacune réalisant
+     * 1000 transferts, dans une banque de 10 comptes. Ne pas traiter l’accès concurrent aux comptes par
+     * les threads pour l’instant
      */
     @Test
     void test1000TransfertInBanqueOf10Accounts() {
@@ -32,6 +33,8 @@ public class TransfertTest {
             transferts.add(transfert);
             new Thread(transfert).start();
         }
+
+        Assertions.assertTrue(banqueTest.consistent());
     }
 
 }

67e676e2e724d10cac9ebc84c00b952d0301861c
Corentin Zeller <contact@corentin-zeller.ch>
2020-03-25 15:32:42 +0100

    module renamed

--- banque/src/test/java/TransfertTest.java
+++ banque/src/test/java/TransfertTest.java
@@ -3,10 +3,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class TransfertTest {
 
